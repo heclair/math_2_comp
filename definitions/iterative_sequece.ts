e6db3e2e4ba82544be0cea8c0c8a862c5c3ad861
unknown <heclair@hotmail.com>
2023-08-23 20:23:45 -0300

    aula23/08

---
+++ definitions/iterative_sequece.ts
@@ -0,0 +1,10 @@
+function sequence2 (n:number){
+
+    let term: number = 1;
+    let limit: number = 2;
+
+    while(limit<n){
+        term = term*2;
+        limit = limit +1;
+    }
+}
---
+++ definitions/recursive_sequence.ts
@@ -0,0 +1,9 @@
+function sequence(n: number): number {
+  if (n == 1) {
+    return 1;
+  } else {
+    return 2 * sequence(n - 1);
+  }
+}
+
+console.log(sequence(10000000));
